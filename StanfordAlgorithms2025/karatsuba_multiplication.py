956c437182a4fd70f83b44a4eae2b973fbc743e7
Maria Kesa <maria.kesa@gmail.com>
2025-05-20 20:03:29 +0300

    remove redundant computation of splitters (divide by //2)

--- StanfordAlgorithms2025/karatsuba_multiplication.py
+++ StanfordAlgorithms2025/karatsuba_multiplication.py
@@ -1,18 +1,19 @@
 def karatsuba(x,y):
     n_digits=len(str(x))
+    m=n_digits//2
     if n_digits==1:
         return x*y
     
-    left_x=x//10**(n_digits//2)
-    right_x=x%10**(n_digits//2)
-    left_y=y//10**(n_digits//2)
-    right_y=y%10**(n_digits//2)
+    left_x=x//10**(m)
+    right_x=x%10**(m)
+    left_y=y//10**(m)
+    right_y=y%10**(m)
 
     first=karatsuba(left_x,left_y)
     second=karatsuba(right_x,right_y)
     third=karatsuba(left_x+right_x,left_y+right_y)-first-second
 
-    return 10**(2*(n_digits//2))*first+10**(n_digits//2)*third+second
+    return 10**(2*m)*first+10**(m)*third+second
 
 def test():
     x=3141592653589793238462643383279502884197169399375105820974944592
